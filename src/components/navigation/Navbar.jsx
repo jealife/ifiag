3139d205a104215012019722d75616d9409fc08c
Jealife <jeanguylanememiaghe@gmail.com>
2024-05-19 18:38:46 +0100

    Changes to be committed: 	modified:   src/components/footer/Footer.jsx 	modified:   src/components/navigation/Navbar.jsx

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -163,7 +163,7 @@ export default function Navbar() {
                 <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto backdrop-blur-xl bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
                         <Link href="#" className="-m-1.5 p-1.5">
-                            <span className="sr-only">TOTAC Academy</span>
+                            <span className="sr-only">IFIAG</span>
                             <img
                                 className="h-8 w-auto"
                                 src={logo}
