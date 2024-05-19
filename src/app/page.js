0c2717ad0800bd07ed76e51148041fac987ec0ac
Jealife <jeanguylanememiaghe@gmail.com>
2024-05-19 20:59:49 +0100

    Changes to be committed: 	modified:   src/app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-dvh flex-col items-center justify-center p-24">
+    <main className="flex min-h-dvh flex-col items-center justify-center">
       {/* <h1 className=" text-5xl font-bold uppercase" >Home</h1> */}
       <FlipWordsDemo/>
     </main>
