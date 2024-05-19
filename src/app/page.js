906028936f248560b09fb6d9bccebbcfbf3b59c2
Jealife <jeanguylanememiaghe@gmail.com>
2024-05-19 20:58:24 +0100

    Changes to be committed: 	modified:   src/app/page.js 	new file:   src/components/FlipWordsDemo/FlipWordsDemo.jsx 	new file:   src/components/ui/flip-words.tsx 	new file:   utils/cn.ts

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import { FlipWordsDemo } from "@/components/FlipWordsDemo/FlipWordsDemo";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-dvh flex-col items-center justify-center p-24">
-      <h1 className=" text-5xl font-bold uppercase" >Home</h1>
+      {/* <h1 className=" text-5xl font-bold uppercase" >Home</h1> */}
+      <FlipWordsDemo/>
     </main>
   );
 }
---
+++ src/components/FlipWordsDemo/FlipWordsDemo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FlipWords } from "../ui/flip-words";
+
+export function FlipWordsDemo() {
+  const words = ["IFIAG","c'est", "la", "Famile"];
+
+  return (
+    <div className="">
+      <h1 className="text-5xl font-semibold text-gray-950 ">
+        <FlipWords words={words} /> 
+      </h1>
+    </div>
+  );
+}
