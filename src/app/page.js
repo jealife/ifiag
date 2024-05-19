27a7fb4b7b447fc11128e978faedcce59129da98
Jealife <jeanguylanememiaghe@gmail.com>
2024-05-19 17:32:21 +0100

    Changes to be committed: 	modified:   src/app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-dvh flex-col items-center justify-center p-24">
       <h1 className=" text-5xl font-bold uppercase" >Home</h1>
     </main>
   );
