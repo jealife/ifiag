f02e4fddbbda6a513050b6087d1e5504d87c7b8c
Jealife <jeanguylanememiaghe@gmail.com>
2024-05-19 17:21:55 +0100

    Changes to be committed: 	modified:   src/components/banner/BannerNav.jsx

--- src/components/banner/BannerNav.jsx
+++ src/components/banner/BannerNav.jsx
@@ -7,11 +7,11 @@ export default function BannerNav() {
         <div className='w-full px-3 py-2  bg-[#F17E3A] flex justify-between text-slate-50'>
             <div className="gap-2 flex text-sm lg:text-xs items-center justify-center ">
                 <div className=" ">
-                    <Link className='flex gap-1 items-center' href="'tel:+212 522-31649"> <FiPhone /> <span className='lg:block hidden'>(+212) 522-31649</span></Link>
+                    <Link className='flex gap-1 items-center' href="tel:+212 522-31649"> <FiPhone /> <span className='lg:block hidden'>(+212) 522-31649</span></Link>
                 </div>
                 <span class="inline-block h-full min-h-full w-px self-stretch bg-white "></span>
                 <div className=" ">
-                    <Link className='flex gap-1 items-center' href="'tel:+212 522-31649"> <FiMapPin />  <span className='lg:block hidden'>Tour des Habous, 50 Avenue des FAR – 1er étage Face a l’hôtel Sheraton, Casablanca</span></Link>
+                    <Link className='flex gap-1 items-center' href="https://maps.app.goo.gl/kbbNrvyAbY7PTtrc6"> <FiMapPin />  <span className='lg:block hidden'>Tour des Habous, 50 Avenue des FAR – 1er étage Face a l’hôtel Sheraton, Casablanca</span></Link>
                 </div>
                 <span class="inline-block h-full min-h-full w-px self-stretch bg-white "></span>
                 <div className=" ">
