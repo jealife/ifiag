531e8f7c4aa4e2aca5b260fb4b579d43d7f2532b
Jealife <jeanguylanememiaghe@gmail.com>
2024-05-21 15:36:50 +0100

    Changes to be committed: 	modified:   src/app/layout.js 	new file:   src/app/logo.png

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,10 +6,19 @@ import BannerNav from "@/components/banner/BannerNav";
 import Navbar from "@/components/navigation/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
-
 export const metadata = {
   title: "IFIAG Vocational School",
-  description: "IFIAG est l’école privée leader de la formation professionnelle supérieure au Maroc depuis 1986",
+  description:
+    "IFIAG est l’école privée leader de la formation professionnelle supérieure au Maroc depuis 1986",
+  openGraph: {
+    icon: "/logo.png",
+    images:
+      "https://ifiag.com/wp-content/uploads/2024/04/encart-site3.png",
+    url: "https://ifiag.vercel.app/",
+  },
+  twitter: {
+    card: "summary_large_image",
+  },
 };
 
 export default function RootLayout({ children }) {
